d8746bc723104f0849611b5f3bb88d1409102fb4
Rajababu Saikia <saikiarajababu@gmail.com>
2020-06-01 21:26:04 -0700

    text data entered

--- client/src/Routes/About/About.js
+++ client/src/Routes/About/About.js
@@ -11,7 +11,7 @@ const Editor = () => {
             <div class="profile-image"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample47.jpg" alt="sample47" /></div>
             <figcaption>
                 <h3>Fleece Marigold</h3>
-                <h4>Founder</h4>
+                <h4>সহযোগী সম্পাদক</h4>
                 <p>Which is worse, that everyone has his price, or that the price is always so low.</p>
                 {/* <div class="icons"><a href="/"><i class="ion-social-reddit"></i></a>
                     <a href="/"> <i class="ion-social-twitter"></i></a>
@@ -37,15 +37,16 @@ const About = () => {
             <Container className={styles.editorcont}>
                 <div className={styles.editorialImage}></div>
                 <div className={styles.desc}>
-                    <h4>Nilkamal Thakuria</h4>
-                    <h6>Editor-in-chief</h6>
-                    <blockquote><i>Nisi reprehenderit occaecat minim Lorem. Fugiat ullamco mollit ea dolor ex incididunt incididunt ea eu ut. Tempor officia irure pariatur cupidatat. Ipsum eu dolor commodo magna voluptate labore tempor dolor magna qui voluptate. Exercitation id officia exercitation pariatur amet nulla consectetur et cupidatat consequat do officia enim. Sit mollit et consequat qui elit nulla do incididunt incididunt qui ullamco. Sit Lorem exercitation velit Lorem pariatur exercitation excepteur pariatur consectetur quis.</i></blockquote>
+                    <h4>জিতুমণি নেওগ</h4>
+                    <h6>মুখ্য সম্পাদক</h6>
+                    <blockquote><i>কৰ’না মহামাৰীয়ে পৃথিৱীলৈ তাৎক্ষণিক পৰিবৰ্তন আনিলে ৷ সলনি কৰি দিলে মানুহৰ জীৱন ধাৰা ৷ এই সময় প্ৰত্যাহ্বানেৰে ভৰা ৷এই সময়ৰ ওপৰত লাহে গজি উঠা মানুহৰ বৰ্তমান আৰু ভবিষ্যবোৰকো লৈ স্পষ্ট ৰেখা অংকন কৰা অসম্ভৱ প্ৰায় ৷ সৌ সিদনা লৈকে বাতৰি কাকত ,আলোচনীৰ অৱিহনে জীয়াই থাকিব নোৱৰা পাঠক সমাজৰ জ্ঞান আৰু তথ্য অহৰণৰ উৎস হৈ পৰিল ডিজিটেল মাধ্যম ৷ অসমৰ কেইবাখনো প্ৰতিষ্ঠিত আলোচনীয়ে ছপা আখৰৰ মুদ্ৰণ বন্ধ কৰি ৰাখিব লগা হৈছে ৷ প্ৰিন্টং প্ৰেছবোৰৰ অৱস্হা তথৈবচ হৈ পৰিছে ৷ কৰ’নাই ছপা আলোচনীৰ যাত্ৰাক যেন স্হবিৰ কৰি পেলালে ৷ ছাত্ৰছাত্ৰী -শিক্ষক- শিক্ষয়িত্ৰী আৰু অভিভাৱকা প্ৰিয় আলোচনী " ন " ৰ প্ৰিন্টিং বন্ধ হৈ পৰিছে ৷ এনে সময়ত পাঠকসকলৰ কাষলৈ যোৱাৰ একমাত্ৰ মাধ্যম হৈ পৰিছে ডিজিটেল প্ৰকাশ ৷ সেয়ে "ন" আলোচনী প্ৰতিজন পাঠকৰ ম’বাইল বা কম্পিউটাৰ স্ক্ৰীনত সুলভ কৰি তুলিবলৈ Thenaaonline.com ৱেবচাইট আৰম্ভ কৰিছো ৷ আগতে আমি মাহত এবাৰ পাঠক ৰাইজৰ কাষলৈ গৈছিলো ৷ এতিয়াৰ পৰা প্ৰতিক্ষণত আমি আপোনালোকৰ কাষত থাকিম ৷ছাত্ৰীছাত্ৰীসকল আগুৱাই নিব পৰাকৈ দৈনিক তথ্য সম্ভাৰ যোগান ধৰিম ৷ সাধাৰণ জ্ঞানৰ পৰা পাঠভিত্তিক জ্ঞান , কেৰিয়াৰ আদি সকলো বিষয় আপোনালকোৰ কাষলৈ নিয়াৰ চেষ্টা কৰিম ৷ "ন"ৰ এই ডিজিটেল যাত্ৰাত আপোনালোকৰ অকুণ্ঠ সহযোগিতা কামনা কৰিলো ৷
+</i></blockquote>
                 </div>
             </Container>
             <Container className="my-5">
                 <h3 className="text-center thedteam">
                     <hr className="dos" />
-                    The Editorial Team
+                    সম্পাদনা কক্ষ
                     <hr className="dos" />
                 </h3>
                 <div className="d-flex mx-auto flex-wrap mt-3 justify-content-center">

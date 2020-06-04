e89e2bee7122520ec1f80fd521aca655582c0e5e
Rajababu Saikia <saikiarajababu@gmail.com>
2020-06-04 13:39:01 -0700

    DATA INPUT

--- client/src/Routes/About/About.js
+++ client/src/Routes/About/About.js
@@ -10,9 +10,9 @@ const Editor = (props) => {
         <figure class="snip1515">
             <div class="profile-image"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample47.jpg" alt="sample47" /></div>
             <figcaption>
-                <h3>Fleece </h3>
-                <h4>সহযোগী সম্পাদক</h4>
-                <p>Which is worse, that everyone has his price, or that the price is always so low.</p>
+                <h3></h3>
+                <h4></h4>
+                <p></p>
                 <h3>{props.name}</h3>
                 <h4>{props.role}</h4>
                 <p>{props.quote}</p>
@@ -30,7 +30,7 @@ const About = () => {
             </Container>
             <h3 className="text-center thedteam my-5">
                 <hr className="dos" />
-                    Message From the Editor-In-Chief
+                মুখ্য সম্পাদকৰ বার্তা
                     <hr className="dos" />
             </h3>
             <Container className={styles.editorcont}>
@@ -38,7 +38,7 @@ const About = () => {
                 <div className={styles.desc}>
                     <h4>জিতুমণি নেওগ</h4>
                     <h6>মুখ্য সম্পাদক</h6>
-                    <blockquote><i>কৰ’না মহামাৰীয়ে পৃথিৱীলৈ তাৎক্ষণিক পৰিবৰ্তন আনিলে ৷ সলনি কৰি দিলে মানুহৰ জীৱন ধাৰা ৷ এই সময় প্ৰত্যাহ্বানেৰে ভৰা ৷এই সময়ৰ ওপৰত লাহে গজি উঠা মানুহৰ বৰ্তমান আৰু ভবিষ্যবোৰকো লৈ স্পষ্ট ৰেখা অংকন কৰা অসম্ভৱ প্ৰায় ৷ সৌ সিদনা লৈকে বাতৰি কাকত ,আলোচনীৰ অৱিহনে জীয়াই থাকিব নোৱৰা পাঠক সমাজৰ জ্ঞান আৰু তথ্য অহৰণৰ উৎস হৈ পৰিল ডিজিটেল মাধ্যম ৷ অসমৰ কেইবাখনো প্ৰতিষ্ঠিত আলোচনীয়ে ছপা আখৰৰ মুদ্ৰণ বন্ধ কৰি ৰাখিব লগা হৈছে ৷ প্ৰিন্টং প্ৰেছবোৰৰ অৱস্হা তথৈবচ হৈ পৰিছে ৷ কৰ’নাই ছপা আলোচনীৰ যাত্ৰাক যেন স্হবিৰ কৰি পেলালে ৷ ছাত্ৰছাত্ৰী -শিক্ষক- শিক্ষয়িত্ৰী আৰু অভিভাৱকা প্ৰিয় আলোচনী " ন " ৰ প্ৰিন্টিং বন্ধ হৈ পৰিছে ৷ এনে সময়ত পাঠকসকলৰ কাষলৈ যোৱাৰ একমাত্ৰ মাধ্যম হৈ পৰিছে ডিজিটেল প্ৰকাশ ৷ সেয়ে "ন" আলোচনী প্ৰতিজন পাঠকৰ ম’বাইল বা কম্পিউটাৰ স্ক্ৰীনত সুলভ কৰি তুলিবলৈ Thenaaonline.com ৱেবচাইট আৰম্ভ কৰিছো ৷ আগতে আমি মাহত এবাৰ পাঠক ৰাইজৰ কাষলৈ গৈছিলো ৷ এতিয়াৰ পৰা প্ৰতিক্ষণত আমি আপোনালোকৰ কাষত থাকিম ৷ছাত্ৰীছাত্ৰীসকল আগুৱাই নিব পৰাকৈ দৈনিক তথ্য সম্ভাৰ যোগান ধৰিম ৷ সাধাৰণ জ্ঞানৰ পৰা পাঠভিত্তিক জ্ঞান , কেৰিয়াৰ আদি সকলো বিষয় আপোনালকোৰ কাষলৈ নিয়াৰ চেষ্টা কৰিম ৷ "ন"ৰ এই ডিজিটেল যাত্ৰাত আপোনালোকৰ অকুণ্ঠ সহযোগিতা কামনা কৰিলো ৷
+                    <blockquote><i>কৰ’না মহামাৰীয়ে পৃথিৱীলৈ তাৎক্ষণিক পৰিবৰ্তন আনিলে ৷ সলনি কৰি দিলে মানুহৰ জীৱন ধাৰা ৷ এই সময় প্ৰত্যাহ্বানেৰে ভৰা ৷এই সময়ৰ ওপৰত লাহে গজি উঠা মানুহৰ বৰ্তমান আৰু ভবিষ্যবোৰকো লৈ স্পষ্ট ৰেখা অংকন কৰা অসম্ভৱ প্ৰায় ৷ সৌ সিদনা লৈকে বাতৰি কাকত ,আলোচনীৰ অৱিহনে জীয়াই থাকিব নোৱৰা পাঠক সমাজৰ জ্ঞান আৰু তথ্য অহৰণৰ উৎস হৈ পৰিল ডিজিটেল মাধ্যম ৷ অসমৰ কেইবাখনো প্ৰতিষ্ঠিত আলোচনীয়ে ছপা আখৰৰ মুদ্ৰণ বন্ধ কৰি ৰাখিব লগা হৈছে ৷ প্ৰিন্টং প্ৰেছবোৰৰ অৱস্হা তথৈবচ হৈ পৰিছে ৷ কৰ’নাই ছপা আলোচনীৰ যাত্ৰাক যেন স্হবিৰ কৰি পেলালে ৷ ছাত্ৰছাত্ৰী -শিক্ষক- শিক্ষয়িত্ৰী আৰু অভিভাৱকা প্ৰিয় আলোচনী " ন " ৰ প্ৰিন্টিং বন্ধ হৈ পৰিছে ৷ এনে সময়ত পাঠকসকলৰ কাষলৈ যোৱাৰ একমাত্ৰ মাধ্যম হৈ পৰিছে ডিজিটেল প্ৰকাশ ৷ সেয়ে "ন" আলোচনী প্ৰতিজন পাঠকৰ ম’বাইল বা কম্পিউটাৰ স্ক্ৰীনত সুলভ কৰি তুলিবলৈ <a href="https://www.thenaaonline.com/">Thenaaonline.com</a> ৱেবচাইট আৰম্ভ কৰিছো ৷ আগতে আমি মাহত এবাৰ পাঠক ৰাইজৰ কাষলৈ গৈছিলো ৷ এতিয়াৰ পৰা প্ৰতিক্ষণত আমি আপোনালোকৰ কাষত থাকিম ৷ছাত্ৰীছাত্ৰীসকল আগুৱাই নিব পৰাকৈ দৈনিক তথ্য সম্ভাৰ যোগান ধৰিম ৷ সাধাৰণ জ্ঞানৰ পৰা পাঠভিত্তিক জ্ঞান , কেৰিয়াৰ আদি সকলো বিষয় আপোনালকোৰ কাষলৈ নিয়াৰ চেষ্টা কৰিম ৷ "ন"ৰ এই ডিজিটেল যাত্ৰাত আপোনালোকৰ অকুণ্ঠ সহযোগিতা কামনা কৰিলো ৷
 </i></blockquote>
                 </div>
             </Container>
@@ -49,18 +49,18 @@ const About = () => {
                     <hr className="dos" />
                 </h3>
                 <div className="d-flex mx-auto flex-wrap mt-3 justify-content-center">
-                    <Editor name="Nilkamal Thakuria" role="Editor" quote="Lorem ipsum dolor sit amet" />
-                    <Editor name="Nilkamal Thakuria" role="Editor" quote="Lorem ipsum dolor sit amet" />
-                    <Editor name="Nilkamal Thakuria" role="Editor" quote="Lorem ipsum dolor sit amet" />
-                    <Editor name="Rajababu Saikia" role="Admin" quote="Loremc hgcsava ytacfsyascf" />
-                    <Editor name="Nilkamal Thakuria" role="Editor" quote="Lorem ipsum dolor sit amet" />
-                    <Editor name="Nilkamal Thakuria" role="Editor" quote="Lorem ipsum dolor sit amet" />
+                    <Editor name="জগত গগৈ" role="সম্পাদক" quote=" " />
+                    <Editor name="মৃদুল ফুকন" role="সহযোগী সম্পাদক" quote=" " />
+                    <Editor name="সমীন শইকীয়া" role="সহযোগী সম্পাদক" quote=" " />
+                    <Editor name="Rajababu Saikia" role="Admin" quote=" " />
+                    <Editor name="দিব্যৰণ্জন বৰা" role="সহযোগী সম্পাদক" quote=" " />
+                    <Editor name="হৰিপ্রসন্ন শইকীয়া" role="সহযোগী সম্পাদক" quote=" " />
                 </div>
             </Container>
             <div className={styles.git}>
                 <h3 className="text-center thedteam">
                     <hr className="dos" />
-                    Get in touch
+                    যোগাযোগ কৰক
                     <hr className="dos" />
                 </h3>
                 <div>
--- client/src/components/utilities/Navbar/Navbar.js
+++ client/src/components/utilities/Navbar/Navbar.js
@@ -6,7 +6,7 @@ const myNavbar = (props) => {
     const { location } = props;
     return (
         <Navbar bg="light" variant="light" expand="lg">
-            <Navbar.Brand href="/">The NAA</Navbar.Brand>
+            <Navbar.Brand href="/">ন - thenaa</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto" activeKey={location.pathname}>
